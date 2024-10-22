83ab4b1698df46fb71a23e2c0a7d128eabf75fb3
MattiaPlacido <placido.mattia@gmail.com>
2024-10-22 15:29:27 +0200

    Inizializzazione repo

---
+++ main.js
@@ -0,0 +1,2 @@
+// Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+// Al click del bottone, la lampadina dovrà accendersi
